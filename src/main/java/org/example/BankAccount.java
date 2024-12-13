e36d0ba6639c09e6f97bf2c50a561347c81fb6a9
yashbim <ybimsara03@gmail.com>
2024-12-13 05:36:12 +0530

    Created variables in BanAccount Class

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class BankAccount {
+    private String account_number;
+    private double balance;
+    private String account_name;
+    private String email;
+    private String contact_number;
 }
