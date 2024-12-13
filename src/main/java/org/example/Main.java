5a3cb2036c04b8210f4795ad440a13bae4b13992
yashbim <ybimsara03@gmail.com>
2024-12-13 05:37:47 +0530

    Created sample instance called account1 in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        BankAccount account1 = new BankAccount();
+        account1.setAccountNumber("00026200100069501");
+        account1.setAccountName("MAYB Madurapperuma");
+        account1.setBalance(100000);
+        account1.setEmail("ybimsara03@gmail.com");
+        account1.setContactNumber("0767329685");
     }
 
 }
