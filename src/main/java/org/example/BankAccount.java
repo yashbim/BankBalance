21056ea49658a0676223711368428b6ad883fcca
yashbim <ybimsara03@gmail.com>
2024-12-13 05:36:55 +0530

    Created getters and setter for variables in BanAccount Class

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -6,4 +6,39 @@ public class BankAccount {
     private String account_name;
     private String email;
     private String contact_number;
+
+    //setter
+    public void setAccountNumber(String accNum) {
+        this.account_number = accNum;
+    }
+    public void setBalance(double accBal) {
+        this.balance = accBal;
+    }
+    public void setAccountName(String accName) {
+        this.account_name = accName;
+    }
+    public void setEmail(String accEmail) {
+        this.email = accEmail;
+    }
+    public void setContactNumber(String accContact) {
+        this.contact_number = accContact;
+    }
+
+    //Getters
+    public String getAccount_number() {
+        return account_number;
+    }
+    public String getAccount_name() {
+        return account_name;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getContact_number() {
+        return contact_number;
+    }
+
 }
