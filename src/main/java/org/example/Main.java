4cee51b1da86acc6b8f11f730643a57c3652d426
yashbim <ybimsara03@gmail.com>
2024-12-15 00:41:08 +0530

    Changed proper print statements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -33,12 +33,18 @@ public class Main {
         try {
             switch (withdrawOrDeposti) {
                 case 'w':System.out.println("Your want to withdraw");
-                    int withdraw_amount = withdrawAmount();
+                    int withdraw_amount = withdrawAmount(accBalance);
                     BankAccount account1 = new BankAccount(accNumber,   accName, accBalance - withdraw_amount, accEmail, accContact);
+                    printAccountDetails(account1);
+                    System.out.println("Withdrawal of Rs." + withdraw_amount + " made." +
+                            " Your account balance is: " + account1.getBalance());
                     return;
                 case 'd':System.out.println("Your want to deposit");
-                    int deposit_amount = depositAmount();
+                    int deposit_amount = depositAmount(accBalance);
                     BankAccount account2 = new BankAccount(accNumber, accName, accBalance + deposit_amount, accEmail, accContact);
+                    printAccountDetails(account2);
+                    System.out.println("Deposit of Rs." + deposit_amount + " made." +
+                            " Your account balance is: " + account2.getBalance());
                 return;
                 default:
                     System.out.println("Invalid withdraw or denomination");
@@ -48,11 +54,9 @@ public class Main {
             System.out.println("Invalid withdraw or denomination");
         }
 
-//        printAccountDetails(account1);
-
     }
 
-    public static int withdrawAmount() {
+    public static int withdrawAmount(double accBalance) {
         Scanner myscanner = new Scanner(System.in);
         System.out.println("Enter amount to withdraw: ");
         int amount = myscanner.nextInt();
@@ -60,7 +64,7 @@ public class Main {
 
     }
 
-    public static int depositAmount() {
+    public static int depositAmount(double accBalance) {
         Scanner myscanner = new Scanner(System.in);
         System.out.println("Enter amount to withdraw: ");
         int amount = myscanner.nextInt();
@@ -69,11 +73,14 @@ public class Main {
     }
 
     public static void printAccountDetails(BankAccount account1) {
-        System.out.println(account1.getAccount_number());
-        System.out.println(account1.getAccount_name());
-        System.out.println(account1.getBalance());
-        System.out.println(account1.getEmail());
-        System.out.println(account1.getContact_number());
+        System.out.println( "BANK STATEMENT" +
+                            "----------------" +
+                            "Account Name : " + account1.getAccount_name() + "\n" +
+                            "Account Number : " + account1.getAccount_number() + "\n" +
+                            "Email : " + account1.getEmail() + "\n" +
+                            "Contact Number : " + account1.getContact_number() + "\n" +
+                            "-----------");
+
     }
 
 }
